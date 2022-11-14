c74b0282362eee2face3c083077818a31ca656be
Emilis Mitrikas <emilis.mitrikas@gmail.com>
2022-11-14 11:27:17 +0000

    Added "GenerateDateString", "GenerateNewName", "CreateDataSet" and "GetProperty" functions

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 const fs = require("fs");
+const path = require("path")
+
+//Generates a date string in the following format: 20221011103552333. You can also define the separator.
+//If you chose ".", the date would look like this:  2022.10.11.10.35.52.333. You cal also omit the milliseconds
+//in which case it will look the same except without the segment
+function GenerateDateString(separator = "", includeMs = true) {
+    let d = new Date()
+
+    const YYYY = d.getUTCFullYear(),
+        MM = separator+(d.getUTCMonth() < 10 ? `0${d.getUTCMonth()}` : d.getUTCMonth()),
+        DD = separator+(d.getUTCDate() < 10 ? `0${d.getUTCDate()}` : d.getUTCDate()),
+        hh = separator+(d.getUTCHours() < 10 ? `0${d.getUTCHours()}` : d.getUTCHours()),
+        mm = separator+(d.getUTCMinutes() < 10 ? `0${d.getUTCMinutes()}` : d.getUTCMinutes()),
+        ss = separator+(d.getUTCSeconds() < 10 ? `0${d.getUTCSeconds()}` : d.getUTCSeconds()),
+        ms = includeMs ? separator+d.getUTCMilliseconds() : ""
+
+    return `${YYYY}${MM}${DD}${hh}${mm}${ss}${ms}`
+}
+
+//Generating a new name perfect for using with creation of random files. Note: existing file check should still
+//be performed as there's a micro chance to produce a duplicate name
+function GenerateNewName(prefix = "", suffix = "", separator = "_") {
+    return `${prefix ? prefix+separator : ""}${GenerateDateString()}${separator}${Math.round(Math.random() * 1000000000000)}${suffix ? separator+suffix : ""}`
+}
 
 //This is analogous to job.createDataset, except, this allows passing on json object
 //directly as a parameter which gets placed into the metadata. In order to do that,
@@ -6,51 +30,44 @@ const fs = require("fs");
 //needs to be removed after "sendTo" is called so that there is no accumulation of
 //unnecessary temp files. To do so, this function returns an object with method "remove()".
 //This method needs to be called after any "sendTo" function in jobArrived.
-async function CreateDataSet(job, datasetName, obj) {
+async function CreateDataSet(job, datasetName, obj, tmp_file_store = "D:/Switch Scripts/_tmp_auto_removal_72h") {
     //Checking whether the right type of variables are supplied to the function
     if (typeof obj !== "object") {
-        job.fail("Type of the variable supplied to createDataset function must be object!");
-        return
+        throw Error(`Expected to receive data type "object", got ${typeof obj}. Dataset can only be created from an object`)
     }
     if (typeof datasetName !== "string" || datasetName === "") {
-        job.fail("DatasetName supplied to createDataset function must be string and at least one character long!");
-        return
+        throw Error(`Dataset name "${datasetName.toString()}" is invalid!`)
     }
 
-    let tmpFile = {
-        //Full path to the location where the json files is stored
-        location: "",
-        //Removes the files temporarily stored in the location
-        remove: () => {
-            fs.unlinkSync(tmpFile.location)
-        },
-        //Generates random name for the file and checks whether it's unique, if not, regenerates it
-        generateNewFileName: () => {
-            for (; ;) {
-                tmpFile.location = "D:/Switch Scripts/_tmp_auto_removal_72h/tmp_" + Date.now() + "_" + Math.floor(Math.random() * 100000000000000000000000000000000000000) + ".json"
+    let location;
 
-                if (fs.existsSync(tmpFile.location)) {
-                    continue
-                }
+    for (;;) {
+        location = path.join(tmp_file_store, `${GenerateNewName("dataset")}.json`);
 
-                break
-            }
-        },
-    };
+        if (fs.existsSync(location)) {continue}
 
-    tmpFile.generateNewFileName();
+        break
+    }
 
-    fs.writeFileSync(tmpFile.location, JSON.stringify(obj))
+    fs.writeFileSync(location, JSON.stringify(obj))
 
-    await job.createDataset(datasetName, tmpFile.location, DatasetModel.JSON);
+    await job.createDataset(datasetName, location, DatasetModel.JSON);
 
-    return tmpFile
+    return {
+        removeTmpFiles: function () {
+            try { fs.unlinkSync(location) } catch {}
+        }
+    }
 }
 
-function Test() {
-    console.log(`WORKING!!!`);
+//Returns property value if name exist or undefined if it doesn't
+async function GetProperty(flowElement, name) {
+    return await flowElement.hasProperty(name) ? await flowElement.getPropertyStringValue(name) : undefined
 }
 
 module.exports = {
-    Test
+    GetProperty,
+    CreateDataSet,
+    GenerateDateString,
+    GenerateNewName
 }
